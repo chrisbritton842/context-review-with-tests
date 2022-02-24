1aab86749fc12c802e820a0f47a924123003b4e7
Chris Britton <chrisbritton790@gmail.com>
2022-02-24 15:43:32 -0800

    finished phase 1

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,19 @@
+import { useCoffeeContext } from "../context/CoffeeContext";
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const context = useCoffeeContext();
+  console.log('Context: ', context)
+  const { coffeeBean } = context;
+  console.log('CoffeeBean: ', coffeeBean)
+
+  return (
+    <div className='selected-coffee'>
+      <h2>
+        {coffeeBean.name}
+      </h2>
+
+    </div>
+  );
 }
 
 export default SelectedCoffeeBean;
